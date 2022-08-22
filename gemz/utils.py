935392574ea1390c81993f787073d32252ccd663
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2022-08-22 18:28:53 +0200

    feat(utils): quantile normalization + files

---
+++ gemz/utils.py
@@ -0,0 +1,71 @@
+"""
+Data pre-processing utils
+"""
+
+import numpy as np
+from scipy.special import ndtri
+
+def average_rank(values, masks, axis=-1):
+    """
+    Average masked ranked of each value, for each given mask, along the given
+    axis of values.
+
+    The average masked rank of a value is the average of the number of masked values
+    less than the given value, and the number of masked values less than or equal
+    to the given value.
+
+    The argument `masks` is simply iterated over with no vectorization benefits,
+    but sorting the values is only done once for all masks, hence the interest
+    of computing several masks at once.
+
+    To get the empirical unregularized CDF, simply divide the resulting ranks by
+    the number of masked values.
+    """
+    sorter = np.argsort(values, axis=axis)
+
+    ranks = [ 0. for _ in masks]
+    for flip in False, True:
+        if flip:
+            _sorter = np.flip(sorter, axis=axis)
+        else:
+            _sorter = sorter
+        sorted_values = np.take_along_axis(values, _sorter, axis=axis)
+
+
+        if not flip:
+            is_new = sorted_values > np.roll(sorted_values, 1, axis=axis)
+        else:
+            is_new = sorted_values < np.roll(sorted_values, 1, axis=axis)
+
+        for i, mask in enumerate(masks):
+            sorted_mask = np.take_along_axis(mask, _sorter, axis=axis)
+            sorted_rank_min = np.maximum.accumulate(
+                    is_new * (
+                        np.cumsum(sorted_mask, axis=axis) - sorted_mask
+                        ),
+                    axis=axis)
+            rank_min = np.empty_like(values)
+            np.put_along_axis(rank_min, _sorter, sorted_rank_min, axis=axis)
+            if flip:
+                rank_min = np.sum(mask, axis=axis) - rank_min
+            ranks[i] += 0.5 * rank_min
+
+    return ranks
+
+def quantile_normalize(values, masks, axis=-1, pseudocounts=0.5):
+    """
+    Quantile normalize the values with quantiles computed only with respect to
+    the masked values.
+
+    The empirical CDF is regularized by assuming extra `pseudocounts` samples at
+    plus and minus infinity, so that values outside of the training support are
+    still mapped to (somewhat arbitrary) finite values.
+    """
+    ranks = average_rank(values, masks, axis=axis)
+
+    return [
+        ndtri(
+            (rank + pseudocounts) / (np.sum(mask, axis=axis) + 2 * pseudocounts)
+            )
+        for mask, rank in zip(masks, ranks)
+        ]
---
+++ tests/test_utils.py
@@ -0,0 +1,97 @@
+"""
+Data preprocessing utils
+"""
+
+import numpy as np
+
+import gemz
+
+def naive_rank_min(values, mask, val):
+    """
+    Number of masked values strictly inferior to val
+    """
+    return sum((values < val) * mask)
+
+def naive_rank_max(values, mask, val):
+    """
+    Number of masked values inferior or equal to val
+    """
+    return sum((values <= val) * mask)
+
+def naive_rank_avg(values, mask, val):
+    """
+    Average of rank_min and rank_mask
+    """
+    return 0.5 * (
+            naive_rank_min(values, mask, val)
+            +
+            naive_rank_max(values, mask, val)
+            )
+
+def test_rank():
+    """
+    Ranking with awareness of ties and masks
+    """
+
+    max_value = 100
+    num_values = 200
+
+    rng = np.random.default_rng(0)
+    values = rng.choice(max_value, size=num_values)
+    mask = rng.choice(2, size=num_values)
+    mask2 = rng.choice(2, size=num_values)
+
+
+    naive_avg = [
+        naive_rank_avg(values, mask, val)
+        for val in values
+        ]
+    naive_avg2 = [
+        naive_rank_avg(values, mask2, val)
+        for val in values
+        ]
+
+    avg, avg2 = gemz.utils.average_rank(values, (mask, mask2))
+
+    assert np.allclose(naive_avg, avg)
+    assert np.allclose(naive_avg2, avg2)
+
+def test_rank_multi():
+    """
+    Vectorized ranking of several arrays at once
+    """
+
+    max_value = 10
+    num_values = 20
+
+    rng = np.random.default_rng(0)
+    values1 = rng.choice(max_value, size=num_values)
+    values2 = rng.choice(max_value, size=num_values)
+    mask = rng.choice(2, size=num_values)
+
+    rank1 = gemz.utils.average_rank(values1, [mask])[0]
+    rank2 = gemz.utils.average_rank(values2, [mask])[0]
+
+    # Stack along first axis, rank along last (default)
+    rank_last, = gemz.utils.average_rank( np.stack((values1, values2)),
+            [mask[None, :]])
+    assert np.allclose(rank_last, np.stack((rank1, rank2)))
+
+    # Stack along last axis, rank along first (less common)
+    rank_first, = gemz.utils.average_rank( np.stack((values1, values2), axis=-1),
+            [mask[:, None]], axis=0)
+    assert np.allclose(rank_first, np.stack((rank1, rank2), axis=-1))
+
+def test_quantile_normalize():
+    """
+    Quantile normalization
+    """
+    qns, = gemz.utils.quantile_normalize(
+            np.array([1, 2, 3]),
+            np.array([[1, 1, 1]])
+            )
+
+    assert qns.shape == (3,)
+    assert -10 < qns[0] < -0.1
+    assert np.isclose(qns[1], 0.)
+    assert 10 > qns[2] > 0.1
