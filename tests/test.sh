02be83dba0ddfc45ee560a21f31b41db837fb411
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2023-03-20 15:27:32 +0100

    Remove container and checkout from test script
    This is now supposed to be already handled by CI and script is called from
    within container after a successful checkout

--- tests/test.sh
+++ tests/test.sh
@@ -1,51 +1,17 @@
 #!/bin/bash
 #
 # General purpose testing script.
-# Can be used to set up various test environment and run parts of the test suite
+# Meant to be called from project root after checkout, will create a temp venv
+# an run tests.
 
 set -eu
 
-case "$1" in
-	singularity)
-		# Run isolated tests in a singularity container
-		# The working directory, which must be the project root, is
-		# bound inside the container, and this script called recursively
-		# inside
-		# As a result, the project root is used as the working dir for
-		# the tests, and a tmp subdirectory is created in it.
-		echo ': Starting container'
-		# This is assumed to have abundant free space for project
-		# clones and environment
-		CONT_TMP=$(mktemp -d)
-		echo "
-		# Commands to run inside singularity
-		# /work is assumed to contain a copy of the project (which is *not* the one
-		# being tested, but usually a bootstrap copy containing this very script)
-		set -eu
-		echo ':: Recursing in container:/work -> host:$(realpath $(pwd)))'
-		cd /work
-		tests/test.sh tmp-clone
-		" | singularity exec -C \
-			-B "$(realpath $(pwd)):/work" \
-			-B "${GH_PAT_FILE}:/pat_file" \
-			-B "${CONT_TMP}:/tmp" \
-			'docker://python:3.10.10' bash
-		;;
-	tmp-clone)
-		DIR=$(mktemp -d)
-		echo "::: Cloning repository"
-		git config --global url."https://ssh:$(</pat_file)@github.com/".insteadOf 'ssh://git@github.com/'
-		git config --global url."https://git:$(</pat_file)@github.com/".insteadOf 'git@github.com:'
-		git clone --recurse-submodules 'git@github.com:emorice/gemz' "$DIR/gemz"
-		echo "::: Creating environment"
-		python -m venv "$DIR/venv"
-		. "$DIR/venv/bin/activate"
-		pip --no-cache-dir install wheel
-		echo "::: Installing dependencies"
-		pip --no-cache-dir install "$DIR/gemz[test]" "$DIR/gemz/gemz-galp[test]"
-		echo "::: Running tests"
-		pytest "--ignore=$DIR" -k 'not peer and not nonlinear_shrinkage' -v
-		;;
-	*)
-		echo "Unknown, or no subcommand given"
-esac
+DIR=$(mktemp -d)
+echo "::: Creating environment"
+python -m venv "$DIR/venv"
+. "$DIR/venv/bin/activate"
+pip --no-cache-dir install wheel
+echo "::: Installing dependencies"
+pip --no-cache-dir install ".[test]" ".gemz-galp[test]"
+echo "::: Running tests"
+pytest "--ignore=$DIR" -k 'not peer and not nonlinear_shrinkage' -v
