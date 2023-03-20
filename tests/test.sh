60dc27e2044c95da346511d2bbb1c4da080c4611
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2023-03-20 15:40:14 +0100

    tests: fix submodule installation

--- tests/test.sh
+++ tests/test.sh
@@ -12,6 +12,6 @@ python -m venv "$DIR/venv"
 . "$DIR/venv/bin/activate"
 pip --no-cache-dir install wheel
 echo "::: Installing dependencies"
-pip --no-cache-dir install ".[test]" ".gemz-galp[test]"
+pip --no-cache-dir install ".[test]" "./gemz-galp[test]"
 echo "::: Running tests"
 pytest "--ignore=$DIR" -k 'not peer and not nonlinear_shrinkage' -v
